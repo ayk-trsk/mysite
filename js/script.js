e74a5f51fa14f04e26d34c8a2a20e874ce7cb7ac
寺坂綾香 <1219noppoppo@gmail.com>
2021-08-28 23:10:01 +0900

    [Add] フェードインアニメーションを追加

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-  $(function(){
-    $('.menu-trigger').on('click',function(event){
-      $(this).toggleClass('active');
-      $('.nav').fadeToggle();
-      event.preventDefault();
-    });
-    $('.nav a[href]').on('click', function(event) {
-      if (window.innerWidth <= 768) {
-        $('.menu-trigger').trigger('click');
-      }
-    });
+$(function(){
+  $('.menu-trigger').on('click',function(event){
+    $(this).toggleClass('active');
+    $('.nav').fadeToggle();
+    event.preventDefault();
   });
+  $('.nav a[href]').on('click', function(event) {
+    if (window.innerWidth <= 768) {
+      $('.menu-trigger').trigger('click');
+    }
+  });
+  $(window).on('load scroll',function (){
+		$('.animation').each(function(){
+			var target = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var height = $(window).height();
+			if (scroll > target - height){
+				$(this).addClass('active');
+			}
+		});
+	});
+});
 
