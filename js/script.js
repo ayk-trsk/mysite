061271077d12b261faae99604466e6ad9b7ba74d
寺坂綾香 <1219noppoppo@gmail.com>
2021-09-13 22:33:36 +0900

    [Add] ロゴアニメーション

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
-$(function(){
-  $('.menu-trigger').on('click',function(event){
+$(function () {
+  $('.menu-trigger').on('click', function (event) {
     $(this).toggleClass('active');
     $('.nav').fadeToggle();
     event.preventDefault();
   });
-  $('.nav a[href]').on('click', function(event) {
+  $('.nav a[href]').on('click', function (event) {
     if (window.innerWidth <= 768) {
       $('.menu-trigger').trigger('click');
     }
   });
-  $(window).on('load scroll',function (){
-		$('.animation').each(function(){
-			var target = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var height = $(window).height();
-			if (scroll > target - height){
-				$(this).addClass('active');
-			}
-		});
-	});
-  $('#back a').on('click',function(event){
+  $(window).on('load scroll', function () {
+    $('.animation').each(function () {
+      var target = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var height = $(window).height();
+      if (scroll > target - height) {
+        $(this).addClass('active');
+      }
+    });
+  });
+  $('#back a').on('click', function (event) {
     $('body,html').animate({
-      scrollTop:0
-    },800);
+      scrollTop: 0
+    }, 800);
     event.preventDefault();
+  });
+  $(function () {
+    setTimeout(function () {
+      $('.start p').fadeIn(1000);
+    }, 500);
+    setTimeout(function () {
+      $('.start').fadeOut(500);
+    }, 2500);
   });
 });
 
